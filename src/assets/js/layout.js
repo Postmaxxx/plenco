9d52a90e943cef918f53b1c2963476b03191a394
Maxim <71179917+Postmaxxx@users.noreply.github.com>
2022-04-16 21:39:52 +0300

    BS Version 0.08 menu creating...

---
+++ src/assets/js/layout.js
@@ -0,0 +1,22 @@
+const navBtnMob = document.querySelector("#nav__btn_mob");
+
+let store = {
+    mobMenuOpened: false,
+}
+
+
+
+function toggleMobMenu(e) {
+    store.mobMenuOpened = !store.mobMenuOpened;
+    console.log(store.mobMenuOpened);
+    if (store.mobMenuOpened) {
+        navBtnMob.classList.add("opened");
+    } else {
+        navBtnMob.classList.remove("opened");
+    }
+}
+
+
+
+
+navBtnMob.addEventListener('click', e => toggleMobMenu(e));
--- web/assets/js/layout.js
+++ web/assets/js/layout.js
@@ -1,2 +1,23 @@
+const navBtnMob = document.querySelector("#nav__btn_mob");
 
+let store = {
+    mobMenuOpened: false,
+}
+
+
+
+function toggleMobMenu(e) {
+    store.mobMenuOpened = !store.mobMenuOpened;
+    console.log(store.mobMenuOpened);
+    if (store.mobMenuOpened) {
+        navBtnMob.classList.add("opened");
+    } else {
+        navBtnMob.classList.remove("opened");
+    }
+}
+
+
+
+
+navBtnMob.addEventListener('click', e => toggleMobMenu(e));
 //# sourceMappingURL=layout.js.map

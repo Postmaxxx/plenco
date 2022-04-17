a039d950245de9a462cd7f8592d285fd05721bea
Maxim <71179917+Postmaxxx@users.noreply.github.com>
2022-04-17 22:47:38 +0300

    BS Version 0.09 menu add creating...

--- web/assets/js/layout.js
+++ web/assets/js/layout.js
@@ -1,7 +1,15 @@
 const navBtnMob = document.querySelector("#nav__btn_mob");
 const navDrop = document.querySelector("#nav_bottom__drop");
+const linksSubMenu = document.querySelectorAll(".nav_bottom__btn_1");
+const blockMarkets = document.querySelector("#nav_bottom__markets");
+const block1 = document.querySelector("#nav_bottom__drop__block-1");
+const block2 = document.querySelector("#nav_bottom__drop__block-2");
+const blockBack = document.querySelector("#nav_bottom__drop__back");
+
+
 let store = {
     mobMenuOpened: false,
+    subMenu: "main"
 }
 
 
@@ -20,6 +28,23 @@ function toggleMobMenu(e) {
 
 
 
+function openSubMenu(submenu) {
+    if (submenu === "markets") {
+        blockMarkets.classList.add("opened")
+        block1.classList.remove("opened");
+        block2.classList.remove("opened");
+        blockBack.classList.add("opened");
+    }
+}
 
-navBtnMob.addEventListener('click', e => toggleMobMenu(e));
+
+navBtnMob.addEventListener("click", e => toggleMobMenu(e));
+
+linksSubMenu.forEach((link) => {
+    if (link.dataset.submenu) {
+        link.addEventListener("click", e => openSubMenu(link.dataset.submenu))
+    }
+})
+
+
 //# sourceMappingURL=layout.js.map

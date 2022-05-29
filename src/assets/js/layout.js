e13a552df3102651c5cfcb2a5d52bfe77ccaf5a4
Maxim <71179917+Postmaxxx@users.noreply.github.com>
2022-05-29 19:05:30 +0300

    links added + modals created + download pages created

--- src/assets/js/layout.js
+++ src/assets/js/layout.js
@@ -27,24 +27,64 @@ const blockCookiesBS = new bootstrap.Collapse(document.querySelector("#block_coo
 
 
 
-//-----------------------------------side menu / modal ----------------------------------
 
-const modalMessageCloseBtn = document.querySelector(".contactUsModal__btn_close");
-const modalMessageSubmitBtn = document.querySelector(".contactUsModal__btn_submit");
-const modalMessageValidateInputs = document.querySelectorAll(".validate");
-const modalMessageBS = new bootstrap.Modal(document.getElementById('contactUsModal'))
+
+
+
+//-----------------------------------side menu / modal contactUs -----------------------------
 const contactUsModal = document.querySelector("#contactUsModal");
+const modalMessageCloseBtn = contactUsModal.querySelector(".contactUsModal__btn_close");
+const modalMessageSubmitBtn = contactUsModal.querySelector(".contactUsModal__btn_submit");
+const modalMessageInputs = contactUsModal.querySelectorAll(".modal-body__input");
+const modalMessageValidateInputs = contactUsModal.querySelectorAll(".validate");
+const modalMessageBS = new bootstrap.Modal(document.getElementById('contactUsModal'));
 
+
+//-----------------------------------side menu / modal email signUp -----------------------------
+const emailSignUpModal = document.querySelector("#emailSignUpModal");
+const modalEmailSignUpCloseBtn = emailSignUpModal.querySelector(".emailSignUpModal__btn_close");
+const modalEmailSignUpBtn = emailSignUpModal.querySelector(".emailSignUpModal__btn_submit");
+const modalEmailSignUpInputs = emailSignUpModal.querySelectorAll(".modal-body__input");
+const modalEmailSignUpValidateInputs = emailSignUpModal.querySelectorAll(".validate");
+const modalemailSignUpBS = new bootstrap.Modal(document.getElementById('emailSignUpModal'));
+
+//-----------------------------------side menu / modal email download -----------------------------
+const emailDownloadModal = document.querySelector("#emailDownloadModal");
+const modalEmailDownloadCloseBtn = emailDownloadModal.querySelector(".emailDownloadModal__btn_close");
+const modalEmailDownloadBtn = emailDownloadModal.querySelector(".emailDownloadModal__btn_submit");
+const modalEmailDownloadInputs = emailDownloadModal.querySelectorAll(".modal-body__input");
+const modalEmailDownloadValidateInputs = emailDownloadModal.querySelectorAll(".validate");
+const modalemailDownloadBS = new bootstrap.Modal(document.getElementById('emailDownloadModal'));
+
+const accemailDownloadModal = emailDownloadModal.querySelector("#accordion-emailDownloadModal");
+let accemailDownloadModalButton = accemailDownloadModal.querySelector("#accordion-emailDownloadModal__button");
+var accemailDownloadModalBody = new bootstrap.Collapse(accemailDownloadModal.querySelector("#acc-emailDownloadModal__body-1"), {
+    toggle: false
+});
 
 //------------------------------- store -----------------------------------------------
 
 let store = { 
     menu: "",
-    showCookies: false,
+    showCookies: true,
     modalContactUS: false,
     cookies_delay: 3000
 }
 
+
+
+//------------------------------- sclose all menus and modal windows------------------------------
+function closeAll() {
+    navDropContMainBS.hide();
+    navDropContMarketsBS.hide();
+    navDropContProductsBS.hide();
+    navProductsBS.hide();
+    navMarketsBS.hide();
+    navSearchBS.hide();
+    navMarketsBtn.classList.remove("opened")
+    navProductsBtn.classList.remove("opened")
+    navBtnMob.classList.remove("opened");
+}
 
 
 //----------------- menu mob---------------------------------------------------
@@ -66,69 +106,57 @@ function toggleMobMenu(menuName) {
 
 
     if (store.menu === "") {
-        navDropContMainBS.hide();
-        navDropContMarketsBS.hide();
-        navDropContProductsBS.hide();
-        navProductsBS.hide();
-        navMarketsBS.hide();
-        navSearchBS.hide();
-        navMarketsBtn.classList.remove("opened")
-        navProductsBtn.classList.remove("opened")
-        navBtnMob.classList.remove("opened");
+        closeAll();
     }
     
     if ((store.menu === "mob_main") || (store.menu === "mob_back")) {
+        closeAll();
         navBtnMob.classList.add("opened");
         navDropContMainBS.show();
-        navDropContMarketsBS.hide();
-        navDropContProductsBS.hide();
     }
 
     if (store.menu === "mob_markets") {
+        closeAll();
         navBtnMob.classList.add("opened");
-        navDropContMainBS.hide();
         navDropContMarketsBS.show();
-        navDropContProductsBS.hide();
     }
 
     if (store.menu === "mob_products") {
+        closeAll();
         navBtnMob.classList.add("opened");  
-        navDropContMainBS.hide();
-        navDropContMarketsBS.hide();
         navDropContProductsBS.show();
     }
 
     if (store.menu === "markets") {
+        closeAll();
         navMarketsBS.show();
-        navProductsBS.hide();
-        navSearchBS.hide();
-        navProductsBtn.classList.remove("opened")
         navMarketsBtn.classList.add("opened")
     }
 
     if (store.menu === "products") {
+        closeAll();
         navProductsBS.show();
-        navMarketsBS.hide();
-        navSearchBS.hide();
-        navMarketsBtn.classList.remove("opened")
         navProductsBtn.classList.add("opened")
     }
 
     if (store.menu === "search") {
+        closeAll();
         navSearchBS.show();
-        navMarketsBS.hide();
-        navProductsBS.hide();
-        navMarketsBtn.classList.remove("opened")
-        navProductsBtn.classList.remove("opened")
     }
 
     if (store.menu === "contact_modal") {
-        modalMessageBS.show()
-        navSearchBS.hide();
-        navMarketsBS.hide();
-        navProductsBS.hide();
-        navMarketsBtn.classList.remove("opened")
-        navProductsBtn.classList.remove("opened")
+        closeAll();
+        modalMessageBS.show();
+    }
+
+    if (store.menu === "email-singup_modal") {
+        closeAll();
+        modalemailSignUpBS.show();
+    }
+
+    if (store.menu === "email-download_modal") {
+        closeAll();
+        modalemailDownloadBS.show();
     }
 
 }
@@ -178,9 +206,9 @@ setTimeout(() => {
 
 
 
-//--------------------------------modal ----------------------------------------------
+//--------------------------------modals ----------------------------------------------
 
-
+//----------------------------------- Contact Us ------------------------------------------
 function modalMessageSubmitClicked(e) {
     
     let correct = true;
@@ -188,11 +216,11 @@ function modalMessageSubmitClicked(e) {
     if (correct) {
         // send form logic should be here
         let sendingData = []
-        modalMessageValidateInputs.forEach((input) => sendingData.push(input.value));
+        modalMessageInputs.forEach((input) => sendingData.push(input.value));
         console.log("sending data: ", sendingData);
         //store.menu = "";
-        alert("All data has been sent!");
-        modalMessageBS.hide()
+        alert("All data has been sent! (Contact Us)");
+        modalMessageBS.hide();
 
     } else {
         console.log('Incorrect sending data, sending canceled');
@@ -210,4 +238,86 @@ contactUsModal.addEventListener("hide.bs.modal", (e) => {
 })
 
 
+//----------------------------------- Email Sign Up ------------------------------------------
 
+function modalEmailSignUpClicked(e) {
+    let correct = true;
+    modalEmailSignUpValidateInputs.forEach(input => !input.validity.valid ? correct = false : null )
+    if (correct) {
+        // send form logic should be here
+        let sendingData = []
+        modalEmailSignUpInputs.forEach((input) => sendingData.push(input.value));
+        console.log("sending data: ", sendingData);
+        //store.menu = "";
+        alert("All data has been sent! (Sign Up)");
+        modalemailSignUpBS.hide();
+    } else {
+        console.log('Incorrect sending data, sending canceled');
+    }
+}
+
+
+modalEmailSignUpBtn.addEventListener("click", (e) => {
+    modalEmailSignUpClicked(e);
+})
+
+
+emailSignUpModal.addEventListener("hide.bs.modal", (e) => {
+    store.menu = "";
+})
+
+
+
+
+//----------------------------------- Email Download ------------------------------------------
+
+function modalEmailDownloadClicked(e) {
+    let correct = true;
+    modalEmailDownloadValidateInputs.forEach(input => !input.validity.valid ? correct = false : null )
+    if (correct) {
+        // send form logic should be here
+        let sendingData = []
+        modalEmailDownloadInputs.forEach((input) => sendingData.push(input.value));
+        console.log("sending data: ", sendingData);
+        //store.menu = "";
+        alert("All data has been sent! (Download)");
+        modalemailDownloadBS.hide();
+    } else {
+        console.log('Incorrect sending data, sending canceled');
+    }
+}
+
+
+modalEmailDownloadBtn.addEventListener("click", (e) => {
+    modalEmailDownloadClicked(e);
+})
+
+emailDownloadModal.addEventListener("hide.bs.modal", (e) => {
+    store.menu = "";
+})
+
+
+function fillInputIndustry(value) {
+    emailDownloadModal.querySelector("#emailDownloadModal-industry").value = value; //fill invisible input with the current/chosen value
+}
+
+ 
+function changeemailDownloadModalValue(e) { // change the header text from the selected item
+    accemailDownloadModalButton.children[0].innerText = e.target.children[0].innerText;
+    fillInputIndustry(accemailDownloadModalButton.children[0].innerText);
+    accemailDownloadModalBody.hide();
+}
+
+
+accemailDownloadModal.addEventListener('click', (e) => {
+    if (e.target.id != "accordion-emailDownloadModal__button") { // if selected !header
+        changeemailDownloadModalValue(e);
+    }
+})
+
+
+fillInputIndustry(accemailDownloadModalButton.children[0].innerText);
+
+
+
+  
